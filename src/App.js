5ef56741275b674ea14940188c13966d2fff398b
Antony Lemos <lemosantony@gmail.com>
2020-10-06 16:48:31 -0300

    🚧 chore: create global style

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+
+import Landing from './pages/Landing';
+
+const App = () => (
+  <>
+    <Landing />
+
+    <GlobalStyle />
+  </>
+);
+
+export default App;
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,43 +1,49 @@
-:root{
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  :root{
     --primary: #ef4242;
     --color-background: #F8F8FC;
     --color-input-background: #F2F0F0;
     --color-card-background: #FAFAFC;
 
     font-size: 60%;
-}
+  }
 
-*{
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-}
-
-html, body, #root{
-    height: 100vh;
-}
-
-body{
-    background: var(--color-background);
-}
-
-input, button, body{
-    font-family: 'Roboto', sans-serif;
-}
-
-button{
     outline: 0;
-}
+  }
 
-#root{
+  html, body, #root{
+    height: 100vh;
+  }
+
+  body {
+    background: var(--color-background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  #root{
     display: flex;
     align-items: center;
     justify-content: center;
-}
+  }
 
-@media (min-width: 700px){
+  @media (min-width: 700px){
     :root{
-        font-size: 67%;
+      font-size: 67%;
     }
-}
-
+  }
+`;
